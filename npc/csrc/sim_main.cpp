b5cc8b33745be7901a148b139b4bc7bed47939f4
2410024020-Cai Weijie <1149187789@qq.com>
2024-10-02 21:41:59 +0800

    Finish the task of Using the NVBoard

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -1,35 +1,38 @@
-  #include "Vtop.h"
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <assert.h>
-  #include "verilated.h"
-  #include "verilated_fst_c.h"
-  int main(int argc, char** argv) {
-      VerilatedContext* contextp = new VerilatedContext;
-      contextp->commandArgs(argc, argv);
-      Vtop* top = new Vtop{contextp};
+#include "Vtop.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "verilated.h"
+#include "verilated_fst_c.h"
+#include <verilated.h>
+#include <nvboard.h>
+
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(TOP_NAME* top);
+
+int main(int argc, char** argv) {
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
+  VerilatedContext* contextp = new VerilatedContext;
+  contextp->commandArgs(argc, argv);
+  Vtop* top = new Vtop{contextp};
       
-      Verilated::traceEverOn(true);
-      VerilatedFstC* tfp = new VerilatedFstC;
-      top->trace(tfp,0);
-      tfp->open("waveform.fst");
+  // Verilated::traceEverOn(true);
+  // VerilatedFstC* tfp = new VerilatedFstC;
+  // top->trace(tfp,0);
+  // tfp->open("waveform.fst");
 
-      int loopCount = 100;
-      while (loopCount >0) {
-	      int a = rand() & 1;
-	      int b = rand() & 1;
-	      top->a = a;
-	      top->b = b;
-	      top->eval();
-	      tfp->dump(contextp->time());
-        contextp->timeInc(1);//仿真时间推进1个时间单位
-	      printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-	      assert(top->f == (a ^ b));
-	      loopCount--;
-      }
-
-      tfp->close();
-      delete top;
-      delete contextp;
-      return 0;
+  int loopCount = 100;
+  while (1) {
+    nvboard_update();
+    dut.eval();
   }
+
+  // tfp->close();
+  delete top;
+  delete contextp;
+  
+  return 0;
+}
--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -1,7 +1,37 @@
+// module top(
+//   input clk,
+//   input rst,
+//   input [1:0]sw,
+//   output led
+// );
+
+// led my_led(
+//   .clk(clk),
+//   .rst(rst),
+//   .sw(sw),
+//   .led(led)
+// );
+// endmodule
+
+// module led(
+//   input clk,
+//   input rst,
+//   input [1:0]sw,
+//   output led
+// );
+//   always @(posedge clk) begin
+//     if (rst) begin led<=1; end
+//     else begin
+//       assign led = sw[0] ^ sw[1];
+//     end
+//   end
+// endmodule
 module top(
   input a,
   input b,
   output f
 );
+
   assign f = a ^ b;
+
 endmodule
